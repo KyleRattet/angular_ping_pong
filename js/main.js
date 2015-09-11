6b6998648f3eb4e8df734c78e61bfba3b72126d7
KyleRattet <krattet@gmail.com>
2015-09-11 07:15:55 -0600

    counters working to keep score, WIP serving show/hide

--- js/main.js
+++ js/main.js
@@ -1,7 +1,46 @@
 // add scripts
 var app = angular.module("myApp", []);
 
-app.controller("PingPong", function($scope){
-  $scope.name = "test";
+app.controller("PlayerOne", function($scope){
+  $scope.scoreOne = 0;
+  var counterOne = 0;
+  $scope.countOne = function () {
+    // console.log('test working');
+    counterOne ++;
+    $scope.scoreOne = counterOne;
+  }
+
+  $scope.scoreTwo = 0;
+  var counterTwo = 0;
+  $scope.countTwo = function () {
+    // console.log('test working');
+    counterTwo ++;
+    $scope.scoreTwo = counterTwo;
+  }
+
+    $scope.serving = "serving";
 })
+
+// app.controller("PlayerTwo", function($scope){
+//   $scope.scoreTwo = 0;
+//   var counter = 0;
+//   $scope.counter = function () {
+//     // console.log('test working');
+//     counter ++;
+//     $scope.scoreTwo = counter;
+//   }
+// })
+
+
+
+// var excitement = 0;
+//   $scope.excite = function () {
+//     excitement ++;
+//     if(excitement>5){
+//       //do something
+//       // console.log("too much excitement");
+//       $scope.greeting = "Class is excited!";
+//     }
+//   }
+
 
