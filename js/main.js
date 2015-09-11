00ed29af6ff5f86bf87a69333cc9f3ceb0591693
KyleRattet <krattet@gmail.com>
2015-09-11 11:19:10 -0600

    serve alernating every two

--- js/main.js
+++ js/main.js
@@ -1,34 +1,112 @@
 // add scripts
 var app = angular.module("myApp", []);
 
-app.controller("PlayerOne", function($scope){
+app.controller("Game", function($scope){
+
+  var serveCount = 0;
+  $scope.servingOne = "";
+  $scope.servingTwo = "serving";
   $scope.scoreOne = 0;
   var counterOne = 0;
   $scope.countOne = function () {
     // console.log('test working');
     counterOne ++;
+    serveCount ++;
     $scope.scoreOne = counterOne;
+    console.log(serveCount);
+    if (serveCount < 2){
+      $scope.servingOne = "";
+      $scope.servingTwo = "serving"
+    } else if(serveCount > 2 && serveCount < 4) {
+      $scope.servingOne = "serving";
+      $scope.servingTwo = "";
+    } else if (serveCount >= 4 && serveCount < 6){
+      $scope.servingOne = "";
+      $scope.servingTwo = "serving";
+    } else if (serveCount >= 6 && serveCount < 8){
+      $scope.servingOne = "serving";
+      $scope.servingTwo = "";
+    } else if (serveCount >= 8 && serveCount < 10){
+      $scope.servingOne = "";
+      $scope.servingTwo = "serving";
+    } else if (serveCount >= 10 && serveCount < 12){
+      $scope.servingOne = "serving";
+      $scope.servingTwo = "";
+    } else if (serveCount >= 12 && serveCount < 14){
+      $scope.servingOne = "";
+      $scope.servingTwo = "serving";
+    } else if (serveCount >= 14 && serveCount < 16){
+      $scope.servingOne = "serving";
+      $scope.servingTwo = "";
+    } else if (serveCount >= 16 && serveCount < 18){
+      $scope.servingOne = "";
+      $scope.servingTwo = "serving";
+    } else {
+      $scope.servingOne = "serving";
+      $scope.servingTwo = "";
+    }
   }
+  // $scope.showServe= function () {
+
+  // }
+
+
+
 
   $scope.scoreTwo = 0;
   var counterTwo = 0;
   $scope.countTwo = function () {
     // console.log('test working');
     counterTwo ++;
+    serveCount ++;
     $scope.scoreTwo = counterTwo;
+    console.log(serveCount);
+     if (serveCount < 2){
+      $scope.servingOne = "";
+      $scope.servingTwo = "serving"
+    } else if(serveCount > 2 && serveCount < 4) {
+      $scope.servingOne = "serving";
+      $scope.servingTwo = "";
+    } else if (serveCount >= 4 && serveCount < 6){
+      $scope.servingOne = "";
+      $scope.servingTwo = "serving";
+    } else if (serveCount >= 6 && serveCount < 8){
+      $scope.servingOne = "serving";
+      $scope.servingTwo = "";
+    } else if (serveCount >= 8 && serveCount < 10){
+      $scope.servingOne = "";
+      $scope.servingTwo = "serving";
+    } else if (serveCount >= 10 && serveCount < 12){
+      $scope.servingOne = "serving";
+      $scope.servingTwo = "";
+    } else if (serveCount >= 12 && serveCount < 14){
+      $scope.servingOne = "";
+      $scope.servingTwo = "serving";
+    } else if (serveCount >= 14 && serveCount < 16){
+      $scope.servingOne = "serving";
+      $scope.servingTwo = "";
+    } else if (serveCount >= 16 && serveCount < 18){
+      $scope.servingOne = "";
+      $scope.servingTwo = "serving";
+    } else {
+      $scope.servingOne = "serving";
+      $scope.servingTwo = "";
+    }
   }
 
-    $scope.serving = "serving";
+  // console.log($scope.total);
+
 })
 
 // app.controller("PlayerTwo", function($scope){
 //   $scope.scoreTwo = 0;
-//   var counter = 0;
-//   $scope.counter = function () {
+//   var counterTwo = 0;
+//   $scope.countTwo = function () {
 //     // console.log('test working');
-//     counter ++;
-//     $scope.scoreTwo = counter;
+//     counterTwo ++;
+//     $scope.scoreTwo = counterTwo;
 //   }
+//   $scope.serving = "serving";
 // })
 
 
